36af42b6cf8f39a8b563d4f47f6ce0b324bce74d
Bruno Alves <vespidhook@gmail.com>
2022-12-27 23:14:30 -0300

    finalizado

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -8,21 +8,21 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
-  cancelledAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
 export class Notification {
   private _id: string;
-
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
-
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
-
       createdAt: props.createdAt ?? new Date(),
     };
   }
@@ -55,8 +55,12 @@ export class Notification {
     return this.props.category;
   }
 
-  public set readAt(readAt: Date | null | undefined) {
-    this.props.readAt = readAt;
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
   }
 
   public get readAt(): Date | null | undefined {
@@ -64,11 +68,11 @@ export class Notification {
   }
 
   public cancel() {
-    this.props.cancelledAt = new Date();
+    this.props.canceledAt = new Date();
   }
 
   public get canceledAt(): Date | null | undefined {
-    return this.props.cancelledAt;
+    return this.props.canceledAt;
   }
 
   public get createdAt(): Date {

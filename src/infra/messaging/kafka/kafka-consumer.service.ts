bd84ac2a89bcdb0cf830937e5687e9508dde415d
Bruno Alves <vespidhook@gmail.com>
2022-12-28 23:03:05 -0300

    iniciando factories

--- src/infra/messaging/kafka/kafka-consumer.service.ts
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -1,5 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { ServerKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class KafkaConsumerService extends ServerKafka {}
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: ['on-tapir-11509-us1-kafka.upstash.io:9092'],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username:
+            'b24tdGFwaXItMTE1MDkkJUwm7el6ubuNFPXsEtyITK5wZKx-AFAb7BN9-_gIGdk',
+          password: '0663cf927a2e4d9f8d6896b881b8845f',
+        },
+        ssl: true,
+      },
+    });
+  }
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { NotificationsController } from './kafka/controllers/notifications.controller';
+import { KafkaConsumerService } from './kafka/kafka-consumer.service';
 
 @Module({
   imports: [],
-  providers: [],
-  controllers: [],
+  providers: [KafkaConsumerService],
+  controllers: [NotificationsController],
 })
 export class MessagingModule {}
